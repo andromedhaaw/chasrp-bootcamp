d7b4dfd4598912a828296d304763e8b9a4ff0753
andromedhaaw <andromedha@students.undip.ac.id>
2025-02-25 10:09:16 +0700

    fizzbuz

---
+++ fizzbuz/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    System.Console.WriteLine("foobar");
+                }
+                else if (i % 3 == 0)
+                {
+                    System.Console.WriteLine("foo");
+                }
+                else if (i % 5 == 0)
+                {
+                    System.Console.WriteLine("bar");
+                }
+                else
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ namespace/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace MyNamespace
+{
+    class Myclass
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello World!");
+        }
+    }
+}
