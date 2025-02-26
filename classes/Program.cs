fcd1ba8d7aed065b35d4da5640d55f542503b309
andromedhaaw <andromedha@students.undip.ac.id>
2025-02-26 09:47:19 +0700

    classes

--- classes/Program.cs
+++ classes/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//membuat kelas bernama mobil
+class Mobil
+{
+    public string merk;
+    public int tahun;
+
+    public void Info()
+    {
+        Console.WriteLine($"Mobil {merk}, Tahun {tahun}");
+    }
+}
+ 
+ // Program Utama   
+class Program
+{
+    static void Main()
+    {
+        Mobil mobilsaya = new Mobil(); // Membuat objek dari kelas Mobil
+        mobilsaya.merk = "Toyota";
+        mobilsaya.tahun = 2002;
+        mobilsaya.Info(); // output: Mobil Toyota, Tahun 2002
+    }
+}
