f2e6127618539b97a3d85c6bea841ff8476067eb
andromedhaaw <andromedha@students.undip.ac.id>
2025-02-26 13:36:10 +0700

    update

--- syntax/obj/Debug/net8.0/syntax.AssemblyInfo.cs
+++ syntax/obj/Debug/net8.0/syntax.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("syntax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d241f0f333e01165dd68a83d85465e509cac5ad3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19f71defe8ff0d35414e7954f320006d54e104da")]
 [assembly: System.Reflection.AssemblyProductAttribute("syntax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("syntax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
