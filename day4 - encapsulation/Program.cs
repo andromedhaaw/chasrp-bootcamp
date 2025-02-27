8748a672d9a081ec79bcca6487eff2f74e3d1cab
andromedhaaw <andromedha@students.undip.ac.id>
2025-02-27 13:12:33 +0700

    encapsulation

---
+++ day4 - encapsulation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Mahasiswa
+{
+    //Field Private (tidak bisa diakses langsung dari luar class)
+    private string nama;
+    private int umur;
+
+    //Properti untuk mengakses nama
+    public string Nama{
+        get { return nama; }
+        set { nama = value; }
+    }
+
+    //properti dengan validasi untuk umur
+    public int Umur{
+        get { return umur;}
+        set 
+        {
+            if (value > 0) // validasi umur tidak boleh negatif
+                umur = value;
+            else
+                Console.WriteLine("Umur harus lebih dari 0!");
+            
+            
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Mahasiswa mhs = new Mahasiswa();
+        mhs.Nama = "Wise";
+        mhs.Umur = 21;
+
+        Console.WriteLine($"Nama: {mhs.Nama}, Umur: {mhs.Umur}");
+    }
+}
