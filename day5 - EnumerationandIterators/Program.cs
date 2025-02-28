e4d81360af44f868cce5eb749a731ab102b92ffe
andromedhaaw <andromedha@students.undip.ac.id>
2025-02-28 14:42:08 +0700

    update day5

---
+++ day5 - EnumerationandIterators/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // 1. Iterasi menggunakan foreach 
+        foreach (char c in "beer")
+        {
+            Console.WriteLine(c);
+        }
+
+        // 2. Collection Initializer 
+        var numbers = new List<int> { 1, 2, 3 };
+        foreach (var num in numbers)
+        {
+            Console.Write(num + " "); // Output: 1 2 3
+        }
+        Console.WriteLine();
+
+        // 3. Iterator dengan yield return 
+        foreach (int fib in Fibs(6))
+        {
+            Console.Write(fib + " "); // Output: 0 1 1 2 3 5
+        }
+        Console.WriteLine();
+    }
+
+    // Iterator: Fibonacci Sequence 
+    static IEnumerable<int> Fibs(int count)
+    {
+        int prev = 0, cur = 1;
+        for (int i = 0; i < count; i++)
+        {
+            yield return prev;
+            int newFib = prev + cur;
+            prev = cur;
+            cur = newFib;
+        }
+    }
+}
---
+++ day5 - operatoroverloading/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public struct Weight : IComparable<Weight>
+{
+    private double kg;
+
+    public Weight(double kilograms) => kg = kilograms;
+
+    // Operator Aritmetika
+    public static Weight operator + (Weight w, double kg) => new Weight(w.kg + kg);
+    public static Weight operator - (Weight w, double kg) => new Weight(w.kg - kg);
+
+    // Operator Perbandingan
+    public static bool operator == (Weight w1, Weight w2) => w1.kg == w2.kg;
+    public static bool operator != (Weight w1, Weight w2) => w1.kg != w2.kg;
+    public static bool operator < (Weight w1, Weight w2) => w1.kg < w2.kg;
+    public static bool operator > (Weight w1, Weight w2) => w1.kg > w2.kg;
+
+    // Konversi ke dan dari gram
+    public static implicit operator double(Weight w) => w.kg * 1000; // Kg ke gram
+    public static explicit operator Weight(double grams) => new Weight(grams / 1000); // Gram ke Kg
+
+    // Implementasi IComparable untuk sorting
+    public int CompareTo(Weight other) => this.kg.CompareTo(other.kg);
+
+    public override string ToString() => $"{kg} kg";
+}
+
+class Program
+{
+    static void Main()
+    {
+        Weight box1 = new Weight(5);
+        Weight box2 = box1 + 3;
+        Weight box3 = box2 - 2;
+
+        Console.WriteLine(box1); // 5 kg
+        Console.WriteLine(box2); // 8 kg
+        Console.WriteLine(box3); // 6 kg
+
+        Console.WriteLine(box1 == box2); // False
+        Console.WriteLine(box1 < box2);  // True
+
+        double grams = box2;
+        Console.WriteLine($"{grams} g"); // 8000 g
+
+        Weight boxFromGram = (Weight)2500;
+        Console.WriteLine(boxFromGram); // 2.5 kg
+    }
+}
