2625fa744e90f82ee9954c565e0417a9fa9004ce
andromedhaaw <andromedha@students.undip.ac.id>
2025-03-12 09:06:11 +0700

    update

--- boolean/obj/Debug/net8.0/boolean.AssemblyInfo.cs
+++ boolean/obj/Debug/net8.0/boolean.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("boolean")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+310a022db12bb18628089491f4bc6e3872a0f0c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0894e41a4ab4e7caf1cdeaa82675f1658d08049d")]
 [assembly: System.Reflection.AssemblyProductAttribute("boolean")]
 [assembly: System.Reflection.AssemblyTitleAttribute("boolean")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ day13 - foobarjazzhuz/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace foobar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 105; // Set the range up to 105
+
+            for (int i = 1; i <= n; i++)
+            {
+                string output = "";
+
+                // Apply the rules in the correct order
+                if (i % 3 == 0)
+                {
+                    output += "foo";
+                }
+                if (i % 4 == 0)
+                {
+                    output += "baz";
+                }
+                if (i % 5 == 0)
+                {
+                    output += "bar";
+                }
+                if (i % 7 == 0)
+                {
+                    output += "jazz";
+                }
+                if (i % 9 == 0)
+                {
+                    output += "huzz";
+                }
+
+                // If output is empty, print the number itself
+                if (output == "")
+                {
+                    Console.Write(i + ", ");
+                }
+                else
+                {
+                    Console.Write(output + ", ");
+                }
+            }
+        }
+    }
+}
