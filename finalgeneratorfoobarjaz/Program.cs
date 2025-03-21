250a926502a7efd1661f88ac046aff9b0e917a05
andromedhaaw <70594403+andromedhaaw@users.noreply.github.com>
2025-03-21 21:28:20 +0700

    update foobar

---
+++ finalgeneratorfoobarjaz/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Foobar
+{
+    class FoobarGenerator
+    {
+        private List<KeyValuePair<int, string>> rules;
+
+        public FoobarGenerator()
+        {
+            rules = new List<KeyValuePair<int, string>>();
+        }
+
+        
+        public void AddRule(int divisor, string output)
+        {
+            rules.Add(new KeyValuePair<int, string>(divisor, output));
+        }
+
+        
+        public void PrintResult(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                string output = "";
+
+               
+                foreach (var rule in rules)
+                {
+                    if (i % rule.Key == 0)
+                    {
+                        output += rule.Value;
+                    }
+                }
+
+                
+                if (output == "")
+                {
+                    Console.Write(i + ", ");
+                }
+                else
+                {
+                    Console.Write(output + ", ");
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 105; 
+            FoobarGenerator generator = new FoobarGenerator();
+
+            
+            generator.AddRule(3, "foo");
+            generator.AddRule(4, "baz");
+            generator.AddRule(5, "bar");
+            generator.AddRule(7, "jazz");
+            generator.AddRule(9, "huzz");
+
+            
+            generator.PrintResult(n);
+        }
+    }
+}
--- fizzbuz/obj/Debug/net8.0/fizzbuz.AssemblyInfo.cs
+++ fizzbuz/obj/Debug/net8.0/fizzbuz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fizzbuz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0894e41a4ab4e7caf1cdeaa82675f1658d08049d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2625fa744e90f82ee9954c565e0417a9fa9004ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("fizzbuz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fizzbuz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
